d6e73c27ecd458c322089920aaa3acd9bf3d1ff2
joshuacassidy <Joshcassidy79@gmail.com>
2017-07-09 13:03:58 +0100

    added labels and a title to the graph

--- Domestic-Gross-By-Genre.py
+++ Domestic-Gross-By-Genre.py
@@ -12,10 +12,14 @@ def plot():
     ax = plt.subplot()
     sns.boxplot(data=data, x='Genre', y='Gross % US',orient='v',  showfliers=False, palette="YlOrRd")
     sns.stripplot(data=data, x='Genre', y='Gross % US',jitter=True,size=6, linewidth=0, hue = 'Studio', alpha=0.7)
-
+    
+    ax.axes.set_title('Domestic Gross by Genre',fontsize=30)
+    ax.set_xlabel('Genre',fontsize=20)
+    ax.set_ylabel('Gross in US (%)',fontsize=20)
     box = ax.get_position()
     ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
     ax.legend(bbox_to_anchor=(1.05,1),loc=2,borderaxespad=0.,frameon=True,fancybox=True,shadow=True,framealpha=1)
+    plt.gcf().canvas.set_window_title('Domestic Gross by Genre (%)')
     plt.show()
 
 plot()
