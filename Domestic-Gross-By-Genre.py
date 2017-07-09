f9b9345523732ecd2b7f4922bf60207497481325
joshuacassidy <Joshcassidy79@gmail.com>
2017-07-09 12:58:21 +0100

    Added styles to the plots and legend

--- Domestic-Gross-By-Genre.py
+++ Domestic-Gross-By-Genre.py
@@ -7,14 +7,15 @@ genre_filters = ['action', 'adventure','animation','comedy','drama' ]
 movieFilters = movieData[movieData.Genre.isin(genre_filters)]
 data = movieFilters[(movieFilters.Studio == 'Buena Vista Studios') | (movieFilters.Studio == 'Fox') | (movieFilters.Studio == 'Paramount Pictures') | (movieFilters.Studio == 'Sony') | (movieFilters.Studio == 'Universal') | (movieFilters.Studio == 'WB')]
 
-ax = plt.subplot()
+def plot():
+    sns.set(style="white",color_codes=True)
+    ax = plt.subplot()
+    sns.boxplot(data=data, x='Genre', y='Gross % US',orient='v',  showfliers=False, palette="YlOrRd")
+    sns.stripplot(data=data, x='Genre', y='Gross % US',jitter=True,size=6, linewidth=0, hue = 'Studio', alpha=0.7)
 
-sns.boxplot(data=data, x='Genre', y='Gross % US',orient='v',  showfliers=False)
+    box = ax.get_position()
+    ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+    ax.legend(bbox_to_anchor=(1.05,1),loc=2,borderaxespad=0.,frameon=True,fancybox=True,shadow=True,framealpha=1)
+    plt.show()
 
-sns.stripplot(data=data, x='Genre', y='Gross % US',jitter=True,size=6, linewidth=0, hue = 'Studio', alpha=0.5)
-
-box = ax.get_position()
-ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
-ax.legend(bbox_to_anchor=(1.05,1),loc=2,borderaxespad=0.)
-
-plt.show()
+plot()
